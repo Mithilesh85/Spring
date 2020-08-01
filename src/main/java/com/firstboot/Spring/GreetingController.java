7b982f99cf09ef63135073b09e960d9a56ab3eeb
Mithilesh shah <58083945+Mithilesh85@users.noreply.github.com>
2020-08-01 15:58:53 +0530

    reverted

--- src/main/java/com/firstboot/Spring/GreetingController.java
+++ src/main/java/com/firstboot/Spring/GreetingController.java
@@ -10,8 +10,12 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+<<<<<<< HEAD
 		//this comment is made after initial commits
 		//changes in mithilesh branch -edit
+=======
+		//this comment is made after initial commit
+>>>>>>> parent of 5e9ef73... this is third commity
 		return "greeting";
 	}
 
